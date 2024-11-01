4378953eb8865d87eb67249c1d3efb33a6a3c987
Abdur Rahman Bilal <aramb@aramservices.com>
2024-11-01 16:49:54 -0400

    changed minLength to 1 for no reason

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const langs = require('langs')
 const colors = require('colors')
 
 const argv = process.argv[2];
-let detected_lang = franc(argv, {minLength: 3})
+let detected_lang = franc(argv, {minLength: 1})
 if (detected_lang === 'und') {
     console.log("Sorry, we couldn't detect the language. Please try again with more text.".red)
 } else {
